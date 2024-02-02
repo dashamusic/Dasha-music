c58b94ca441fe6e74d684aff622afb30d209ea0f
dashamusic <158588894+dashamusic@users.noreply.github.com>
2024-02-02 20:38:23 +0530

    Update config.py
    Signed-off-by: dashamusic <158588894+dashamusic@users.noreply.github.com>

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Ethical_Botz")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/dashaAssociation")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/dashabotsupport")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
